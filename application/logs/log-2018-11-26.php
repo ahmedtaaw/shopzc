3755b948cb8e23438ee65c7c578edc2c890a0c41
ahmedtaaw <ahmedtaaw@gmail.com>
2018-11-30 20:35:45 +0200

    shopzc init

---
+++ application/logs/log-2018-11-26.php
@@ -0,0 +1,17 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-11-26 20:03:29 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) C:\xampp\htdocs\shop\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2018-11-26 20:03:29 --> Unable to connect to the database
+ERROR - 2018-11-26 20:21:34 --> Severity: Error --> Maximum execution time of 30 seconds exceeded C:\xampp\htdocs\shop\system\core\Common.php 595
+ERROR - 2018-11-26 20:30:47 --> Severity: Error --> Maximum execution time of 30 seconds exceeded C:\xampp\htdocs\shop\system\core\Common.php 595
+ERROR - 2018-11-26 20:30:58 --> Severity: Error --> Maximum execution time of 30 seconds exceeded C:\xampp\htdocs\shop\system\core\Common.php 595
+ERROR - 2018-11-26 20:31:08 --> Severity: Error --> Maximum execution time of 30 seconds exceeded C:\xampp\htdocs\shop\system\core\Common.php 595
+ERROR - 2018-11-26 20:31:31 --> Severity: Error --> Maximum execution time of 30 seconds exceeded C:\xampp\htdocs\shop\system\core\Common.php 595
+ERROR - 2018-11-26 20:35:13 --> Severity: Error --> Maximum execution time of 30 seconds exceeded C:\xampp\htdocs\shop\system\core\Common.php 595
+ERROR - 2018-11-26 20:37:01 --> Severity: Error --> Maximum execution time of 30 seconds exceeded C:\xampp\htdocs\shop\system\core\Common.php 595
+ERROR - 2018-11-26 20:40:27 --> Severity: Error --> Maximum execution time of 30 seconds exceeded C:\xampp\htdocs\shop\system\core\Common.php 595
+ERROR - 2018-11-26 20:42:52 --> Severity: Error --> Maximum execution time of 30 seconds exceeded C:\xampp\htdocs\shop\system\core\Common.php 595
+ERROR - 2018-11-26 20:46:57 --> Severity: Error --> Maximum execution time of 30 seconds exceeded C:\xampp\htdocs\shop\system\core\Common.php 595
+ERROR - 2018-11-26 20:51:43 --> Severity: Error --> Maximum execution time of 30 seconds exceeded C:\xampp\htdocs\shop\system\core\Common.php 595
+ERROR - 2018-11-26 20:52:12 --> Severity: Error --> Maximum execution time of 30 seconds exceeded C:\xampp\htdocs\shop\system\core\Common.php 595
+ERROR - 2018-11-26 20:56:03 --> Severity: Error --> Maximum execution time of 30 seconds exceeded C:\xampp\htdocs\shop\system\core\Common.php 595
